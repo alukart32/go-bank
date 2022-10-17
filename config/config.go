6c67add7ad9055dcc73288af9d1388ddc572de0d
Ivan Tarasov <alukart32work@gmail.com>
2022-10-17 13:53:33 +0300

    [REF] Change env lib.

--- config/config.go
+++ config/config.go
@@ -1,64 +1,71 @@
 package config
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"regexp"
 	"time"
 
-	"github.com/joho/godotenv"
+	"github.com/ilyakaznacheev/cleanenv"
 	"gopkg.in/yaml.v3"
 )
 
-const projectDirName = "bank"
+const (
+	projectDirName = "bank"
+	Default        = "default"
+	Test           = "test"
+)
 
 type (
 	DB struct {
-		URI         string `json:"uri" yaml:"uri"`
-		Driver      string
-		DBname      string `json:"dbname" yaml:"dbname"`
-		InitTimeout time.Duration
+		URI    string `env-required:"true" env:"DB_URI"`
+		Driver string `env:"DB_DRIVER" env-default:"postgres"`
+		DBname string `env-required:"true" env:"DB_NAME"`
 	}
 
+	HTTP struct {
+		Port            string        `env:"HTTP_PORT" env-default:"80"`
+		ReadTimeout     time.Duration `env:"HTTP_READ_TIMEOUT" env-default:"5000us"`
+		WriteTimeout    time.Duration `env:"HTTP_WRITE_TIMEOUT" env-default:"5000us"`
+		ShutdownTimeout time.Duration `env:"HTTP_SHUTDOWN_TIMEOUT" env-default:"3000us"`
+	}
 	Logger struct {
 	}
 
 	Config struct {
 		DB     DB
+		Http   HTTP
 		Logger Logger
 	}
 )
 
+var (
+	profiles = map[string]string{
+		"test":    "test.env",
+		"default": ".env",
+	}
+)
+
 func New(profile string) (*Config, error) {
-	if len(profile) == 0 {
-		profile = getEnvPath() + "/config/.env"
-	} else {
-		profile = getEnvPath() + "/config/" + profile + ".env"
+	cfg := &Config{}
+
+	cfgPath := getRootDir() + "/config" + profiles["default"]
+	if f, exist := profiles[profile]; exist {
+		cfgPath = getRootDir() + "/config/" + f
 	}
 
-	err := godotenv.Load(profile)
+	err := cleanenv.ReadConfig(cfgPath, cfg)
+	if err != nil {
+		return nil, fmt.Errorf("config error: %w", err)
+	}
+
+	err = cleanenv.ReadEnv(cfg)
 	if err != nil {
 		return nil, err
 	}
 
-	db := &DB{
-		URI:         os.Getenv("PGDB_URI"),
-		Driver:      os.Getenv("DB_DRIVER"),
-		DBname:      os.Getenv("PGDB_NAME"),
-		InitTimeout: 25 * time.Microsecond,
-	}
-
-	return &Config{
-		DB: *db,
-	}, nil
-}
-
-// GetEnvPath define env vars from .env
-func getEnvPath() string {
-	re := regexp.MustCompile(`^(.*` + projectDirName + `)`)
-	cwd, _ := os.Getwd()
-	rootPath := re.Find([]byte(cwd))
-	return string(rootPath)
+	return cfg, nil
 }
 
 // Print outputs the configuration in YAML format.
@@ -68,4 +75,12 @@ func Print(c *Config) {
 	} else {
 		log.Printf("config data\n%s%v", "---\n", string(data))
 	}
+}
+
+// getRootDir get project root dir.
+func getRootDir() string {
+	re := regexp.MustCompile(`^(.*` + projectDirName + `)`)
+	cwd, _ := os.Getwd()
+	rootPath := re.Find([]byte(cwd))
+	return string(rootPath)
 }
