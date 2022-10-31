72225131b7b610aa76a755a7d33c72460aa36c1a
Ivan Tarasov <alukart32work@gmail.com>
2022-10-31 10:27:13 +0300

    [DEV] Add docker compose.

---
+++ deployments/docker_postgres_init.sql
@@ -0,0 +1,37 @@
+CREATE DATABASE bank
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.utf8'
+    LC_CTYPE = 'en_US.utf8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+
+GRANT ALL PRIVILEGES ON DATABASE bank TO postgres;
+
+CREATE DATABASE bank_test
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.utf8'
+    LC_CTYPE = 'en_US.utf8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+
+GRANT ALL PRIVILEGES ON DATABASE bank_test TO postgres;
+
+CREATE USER keycloak
+    WITH
+    ENCRYPTED PASSWORD 'keycloak'
+    INHERIT;
+
+CREATE DATABASE keycloak
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.utf8'
+    LC_CTYPE = 'en_US.utf8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+
+GRANT ALL PRIVILEGES ON DATABASE keycloak TO keycloak;
